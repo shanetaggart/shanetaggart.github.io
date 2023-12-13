350e0c8bd4a30cd2dfc42417206211fb15698150
Shane Taggart <staggart@eastlink.ca>
2023-12-12 21:08:17 -0400

    Site design version 1, still some cleaning up and tweaking to do.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/main.js',
+  mode: 'none',
+  entry: './src/js/main.js',
   output: {
     filename: 'main.min.js',
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, 'public/js/'),
   },
 };
