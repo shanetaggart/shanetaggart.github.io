5057e30266aa4abc3757ecc0149a647c27185304
Shane Taggart <staggart@eastlink.ca>
2023-12-14 11:57:10 -0400

    Converted HTML to PHP files for compartmentalization, added some standard and OGP meta elements.

---
+++ includes/head.php
@@ -0,0 +1,33 @@
+<head>
+    <!-- Charset and Viewport declarations -->
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- Standard metadata -->
+    <title>Shane Taggart</title>
+    <meta name="description" content="Shane Taggart is a software developer who builds elegant and performant web applications." />
+    <meta name="author" content="Shane Taggart" />
+    <meta name="application-name" content="Shane Taggart" />
+
+    <!-- OGP metadata -->
+    <meta property="og:title" content="Shane Taggart" />
+    <meta property="og:description" content="Shane Taggart is a software developer who builds elegant and performant web applications." />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://shanetaggart.github.io/" />
+    <meta property="og:image" content="https://shanetaggart.github.io/public/assets/images/profile-picture.jpeg" />
+    <meta property="og:image:alt" content="Shane Taggart's profile picture." />
+    <meta property="og:locale" content="en_CA" />
+
+    <meta name="twitter:card" content="https://shanetaggart.github.io/public/assets/images/profile-picture.jpeg" />
+    <meta property="twitter:domain" content="shanetaggart.github.io" />
+    <meta property="twitter:url" content="https://shanetaggart.github.io/" />
+    <meta name="twitter:title" content="Shane Taggart" />
+    <meta name="twitter:description" content="Shane Taggart is a software developer who builds elegant and performant web applications." />
+    <meta name="twitter:image" content="https://shanetaggart.github.io/public/assets/images/profile-picture.jpeg" />
+
+    <!-- Preloads and Assets -->
+    <link rel="preload" href="public/css/main.css" as="style" />
+    <link rel="preload" href="public/js/main.min.js" as="script" />
+    <link rel="preload" href="public/assets/images/profile-picture.jpeg" as="image" />
+    <link rel="stylesheet" type="text/css" href="public/css/main.css" />
+</head>
